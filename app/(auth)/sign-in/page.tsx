38749089d3dda1a777bd627ff976273fde0b409c
lasmor <llincohse@gmail.com>
2025-11-17 17:35:32 +0100

    implement Auth UI & Layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import {useForm} from 'react-hook-form';
+import {Button} from '@/components/ui/button';
+import InputField from '@/components/forms/inputField';
+import FooterLink from '@/components/FooterLink';
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
+const SignIn = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+    } = useForm<SignInFormData>({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        mode: 'onBlur',
+    });
+    const onsubmit = async (data: SignInFormData) => {
+        try {
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <>
+            <h1 className="form-title">Welcome back</h1>
+
+            <form onSubmit={handleSubmit(onsubmit)} className="space-y-5">
+                <InputField
+                    name="email"
+                    label="Email"
+                    placeholder="contact@jsmastery.com"
+                    register={register}
+                    error={errors.email}
+                    validation={{required: 'Email is required', pattern: /^\w+@\w+\.\w+$/}}
+                />
+
+                <InputField
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    register={register}
+                    error={errors.password}
+                    validation={{required: 'Password is required', minLength: 8}}
+                />
+
+                <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                    {isSubmitting ? 'Signing In' : 'Sign In'}
+                </Button>
+
+                <FooterLink text="Don't have an account?" linkText="Create an account" href="/sign-up"/>
+            </form>
+        </>
+    )
+}
+export default SignIn
